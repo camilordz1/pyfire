063f124fa4c502ef8e074d55d40fd462a5ca4fd5
Camilo Andres Rodriguez <andres.roh@gmail.com>
2023-06-09 16:39:47 -0500

    Added .env file path

--- pyfiredb/settings.py
+++ pyfiredb/settings.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
 from environs import Env
+from pathlib import Path
 import json
 
 
 env = Env()
-env.read_env()
+env.read_env(Path.cwd()/".env")
 
 
 @dataclass
