ea206ab866ade553450573fbc3e4fdb4ce22b00a
Camilo Andres Rodriguez <andres.roh@gmail.com>
2023-06-18 22:14:38 -0500

    Improved reading of environment variables to store them if they are not assigned

--- pyfiredb/settings.py
+++ pyfiredb/settings.py
@@ -3,6 +3,9 @@ from environs import Env
 from pathlib import Path
 import json
 
+if not (Path.cwd()/".env").exists():
+    (Path.cwd()/".env").touch()
+
 
 env = Env()
 env.read_env(Path.cwd()/".env")
@@ -10,10 +13,26 @@ env.read_env(Path.cwd()/".env")
 
 @dataclass
 class Settings:
-    setup = json.loads(env("DB_SETUP"))
+    try:
+        setup = json.loads(env("DB_SETUP"))
+    except Exception:
+        with open(Path.cwd()/".env", "a") as env:
+            db_setup = input("DB_SETUP")
+            env.writelines("DB_SETUP="+db_setup+"\n")
 
 
 @dataclass
 class Credentials:
-    user = env("DB_USER")
-    password = env("DB_PASS")
+    try:
+        user = env("DB_USER")
+    except Exception:
+        with open(Path.cwd()/".env", "a") as env:
+            user = input("DB_USER")
+            env.writelines("DB_USER="+user+"\n")
+
+    try:
+        password = env("DB_PASS")
+    except Exception:
+        with open(Path.cwd()/".env", "a") as env:
+            user = input("DB_PASS")
+            env.writelines("DB_PASS="+user+"\n")
